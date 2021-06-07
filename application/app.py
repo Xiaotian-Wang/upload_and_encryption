21d55f5439fdf1cd98af9056afd6dab6e429c5ef
Xiaotian-Wang <wangxt96@126.com>
2021-06-07 16:32:30 +0800

    功能已实现

--- application/app.py
+++ application/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, flash, redirect, url_for, request
+from flask import Flask, flash, redirect, url_for, request, render_template, send_from_directory, make_response
 from werkzeug.utils import secure_filename
 import os
 import service
+import pandas as pd
 
-UPLOAD_FOLDER = './data/data'
+UPLOAD_FOLDER = './data/data/'
 
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xls', 'xlsx'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xls', 'xlsx', 'py'}
+
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000000
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -44,38 +46,52 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+            return redirect(url_for('upload_file'))
+        file = request.files.get('file')
+        thekey = request.form.get('pass')
+        thekey = thekey+'0'*(16-len(thekey))
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            service.encrypt_file1(key=thekey.encode(), in_filename=file, out_filename=filename+'.enc', out_path=UPLOAD_FOLDER)
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with open('data/data_info.csv','a+') as f:
+                f.writelines('\r'+filename+'.enc'+','+UPLOAD_FOLDER+filename+'.enc'+','+thekey)
             return redirect(url_for('upload_file'))
             # return 'success'
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('template1.html')
+
+
+@app.route('/file_list', methods=['GET', 'POST'])
+def list():
+    if request.method == 'GET':
+        data_info = pd.read_csv('data/data_info.csv')
+        file_list = []
+        for i in range(len(data_info)):
+            file_list.append({'name':data_info['name'][i], 'path': data_info['path'][i]})
+        return render_template('template2.html', file_list=file_list)
+    elif request.method == 'POST':
+        pwd = request.form.get('password')
+        fname = request.form.get('filename')
+        thekey = pwd+'0'*(16-len(pwd))
+        data_path = 'data/data/'
+        fname_path = data_path+fname
+        out_filename = 'temp/'+os.path.splitext(fname)[0]
+        service.decrypt_file(key=thekey.encode(), in_filename=fname_path, out_filename=out_filename)
+        response = make_response(send_from_directory('', out_filename, as_attachment=True))
+        return response
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def return_file(filename):
+    directory = 'data/data/'
+    response = make_response(send_from_directory(directory, filename, as_attachment=True))
+    return response
 
 
 if __name__ == '__main__':
     app.run()
 
-'''
-import pandas as pd
-data = pd.DataFrame()
-data['name'] = ['graphdata(new).xlsx']
-data['path'] = [r'./data/data/graphdata(new).xlsx']
-data['key'] = ['sample_password']
-data.to_csv('data/data_info.csv',index=False)
-'''
